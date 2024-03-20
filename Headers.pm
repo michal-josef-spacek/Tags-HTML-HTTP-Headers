73c6284c569ecde441b252282b231475b5c5764a
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2024-03-20 17:44:15 +0100

    Next version

--- Headers.pm
+++ Headers.pm
@@ -1,6 +1,6 @@
 package Tags::HTML::HTTP::Headers;
 
-use parent 'Tags::HTML';
+use parent qw(Tags::HTML);
 use strict;
 use warnings;
 
@@ -10,6 +10,8 @@ use List::Util qw(any);
 use Readonly;
 use Scalar::Util qw(blessed);
 
+Readonly::Array our @TYPES => ('cache', 'client_info', 'connection', 'control', 'login',
+	'security', '');
 Readonly::Hash our %TYPES => (
 	'cache' => [
 		'Cache-Control',
@@ -22,6 +24,24 @@ Readonly::Hash our %TYPES => (
 		'Last-Modified',
 		'Pragma',
 		'Vary',
+	],
+	'client_info' => [
+		'DNT',
+		'From',
+		'Referer',
+		'User-Agent',
+		'X-Forwarded-For',
+		'X-Forwarded-Host',
+		'X-Forwarded-Proto',
+		'X-Requested-With',
+	],
+	'connection' => [
+		'Connection',
+		'Host',
+		'Keep-Alive',
+		'TE',
+		'Transfer-Encoding',
+		'Upgrade',
 	],
 	'control' => [
 		'Content-Encoding',
@@ -40,11 +60,42 @@ Readonly::Hash our %TYPES => (
 		'Set-Cookie',
 		'WWW-Authenticate',
 	],
+	'negotiation' => [
+		'Accept',
+		'Accept-Language',
+		'Accept-Encoding',
+	],
+	'security' => [
+		'Content-Security-Policy',
+		'Cross-Origin-Embedder-Policy',
+		'Cross-Origin-Opener-Policy',
+		'Cross-Origin-Resource-Policy',
+		'Feature-Policy',
+		'Permissions-Policy',
+		'Referrer-Policy',
+		'Sec-Ch-Ua',
+		'Sec-Ch-Ua-Platform',
+		'Sec-Ch-Ua-Mobile',
+		'Sec-Fetch-Dest',
+		'Sec-Fetch-Mode',
+		'Sec-Fetch-Site',
+		'Sec-Fetch-User',
+		'Sec-WebSocket-Key',
+		'Strict-Transport-Security',
+		'Upgrade-Insecure-Requests',
+		'X-Content-Type-Options',
+		'X-Frame-Options',
+		'X-XSS-Protection',
+	],
 );
 Readonly::Hash our %COLORS => (
-	'cache' => 'bisque',
+	'cache' => 'SandyBrown',
+	'client_info' => 'Fuchsia',
+	'connection' => 'charTReuse',
 	'control' => 'turquoise',
 	'login' => 'indigo',
+	'negotiation' => 'Cyan',
+	'security' => 'darkgreen',
 );
 
 our $VERSION = 0.01;
@@ -57,17 +108,37 @@ sub _cleanup {
 	return;
 }
 
-sub _find_color {
-	my ($self, $header_key) = @_;
+sub _headers {
+	my $self = shift;
 
-	my $color = 'black';
-	foreach my $type_group (keys %TYPES) {
-		if (any { $header_key eq $_ } @{$TYPES{$type_group}}) {
-			$color = $COLORS{$type_group};
+	my %headers;
+	if (ref $self->{'_headers'} eq 'HTTP::Headers::Fast') {
+		%headers = @{$self->{'_headers'}->psgi_flatten};
+	} else {
+		%headers = $self->{'_headers'}->flatten;
+	}
+
+	my $ret_headers_hr = {};
+	foreach my $header_key (sort keys %headers) {
+		my $none = 1;
+		foreach my $type_group (keys %TYPES) {
+			if (any { $header_key eq $_ } @{$TYPES{$type_group}}) {
+				if (! exists $ret_headers_hr->{$type_group}) {
+					$ret_headers_hr->{$type_group} = [];
+				}
+				push @{$ret_headers_hr->{$type_group}},
+					[$header_key, $headers{$header_key}];
+				$none = 0;
+				last;
+			}
+		}
+		if ($none) {
+			push @{$ret_headers_hr->{''}},
+				[$header_key, $headers{$header_key}];
 		}
 	}
 
-	return $color;
+	return $ret_headers_hr;
 }
 
 sub _init {
@@ -88,30 +159,36 @@ sub _init {
 sub _process {
 	my $self = shift;
 
-	my %headers;
-	if (ref $self->{'_headers'} eq 'HTTP::Headers::Fast') {
-		%headers = @{$self->{'_headers'}->psgi_flatten};
-	} else {
-		%headers = $self->{'_headers'}->flatten;
-	}
 	$self->{'tags'}->put(
 		['b', 'div'],
 		['a', 'class', 'headers'],
 	);
-	foreach my $header_key (keys %headers) {
-		my $color = $self->_find_color($header_key);
-		$self->{'tags'}->put(
-			['b', 'span'],
-			['a', 'style', 'color: '.$color.';'],
-			['d', $header_key],
-			['e', 'span'],
-			['d', ':'],
-			['b', 'span'],
-			['d', $headers{$header_key}],
-			['e', 'span'],
-			['b', 'br'],
-			['e', 'br'],
-		);
+	my $headers_hr = $self->_headers;
+	my $num = 0;
+	foreach my $type (@TYPES) {
+		my $color = 'black';
+		if (exists $COLORS{$type}) {
+			$color = $COLORS{$type};
+		}
+		foreach my $header_ar (@{$headers_hr->{$type}}) {
+			if ($num) {
+				$self->{'tags'}->put(
+					['b', 'br'],
+					['e', 'br'],
+				);
+			}
+			$self->{'tags'}->put(
+				['b', 'span'],
+				['a', 'style', 'color: '.$color.';'],
+				['d', $header_ar->[0]],
+				['e', 'span'],
+				['d', ':'],
+				['b', 'span'],
+				['d', $header_ar->[1]],
+				['e', 'span'],
+			);
+			$num = 1;
+		}
 	}
 	$self->{'tags'}->put(
 		['e', 'div'],
@@ -122,6 +199,16 @@ sub _process {
 
 sub _process_css {
 	my $self = shift;
+
+	$self->{'css'}->put(
+		['s', '.headers'],
+		['d', 'margin', '1em'],
+		['d', 'padding', '1em'],
+		['d', 'border-radius', '8px'],
+		['d', 'box-shadow', '0 2px 4px rgba(0, 0, 0, 0.1)'],
+		['d', 'background-color', '#f4f4f4'],
+		['e'],
+	);
 
 	return;
 }
@@ -143,7 +230,9 @@ Tags::HTML::HTTP::Headers - Tags helper for gradient evaluation.
  use Tags::HTML::HTTP::Headers;
 
  my $obj = Tags::HTML::HTTP::Headers->new(%params);
- $obj->process($stars_hr);
+ $obj->cleanup;
+ $obj->init($headers);
+ $obj->process;
  $obj->process_css;
 
 =head1 METHODS
@@ -164,36 +253,6 @@ It's required.
 
 Default value is undef.
 
-=item * C<css_background_image>
-
-CSS parameter for background-image of gradient.
-
-Default value is 'linear-gradient(to right, red, orange, yellow, green, blue, indigo, violet)'.
-
-=item * C<css_gradient_class>
-
-CSS class name for gradient.
-
-Default value is 'gradient'.
-
-=item * C<height>
-
-Indicator height.
-
-Default value is 30.
-
-=item * C<unit>
-
-Unit for height and width.
-
-Default value is 'px'.
-
-=item * C<width>
-
-Indicator width.
-
-Default value is 500.
-
 =item * C<tags>
 
 'Tags::Output' object.
@@ -202,11 +261,36 @@ Default value is undef.
 
 =back
 
+Returns instance of object.
+
+=head2 C<cleanup>
+
+ $obj->cleanup;
+
+Process cleanup after page run.
+
+In this case cleanup internal representation of a set by L<init>.
+
+Returns undef.
+
+=head2 C<init>
+
+ $obj->init($headers);
+
+Process initialization in page run.
+
+Accepted C<$headers> is L<HTTP::Headers> object.
+
+There is difference in C<flatten()> method of L<HTTP::Headers> and
+C<psgi_flatten()> method of L<HTTP::Headers::Fast>. Second one is accepted too.
+
+Returns undef.
+
 =head2 C<process>
 
- $obj->process($percent_value);
+ $obj->process;
 
-Process Tags structure for gradient.
+Process Tags structure for HTTP headers.
 
 Returns undef.
 
@@ -226,12 +310,13 @@ Returns undef.
          Parameter 'css' must be a 'CSS::Struct::Output::*' class.
          Parameter 'tags' must be a 'Tags::Output::*' class.
 
-=head1 EXAMPLE1
+=head1 EXAMPLE
 
  use strict;
  use warnings;
 
  use CSS::Struct::Output::Indent;
+ use HTTP::Headers;
  use Tags::HTML::HTTP::Headers;
  use Tags::Output::Indent;
 
@@ -243,9 +328,18 @@ Returns undef.
          'tags' => $tags,
  );
 
- # Process indicator.
+ # Example Header.
+ my $headers = HTTP::Headers->new(
+         'Host' => 'example.com',
+         'Content-Type' => 'text/plain',
+ );
+
+ # Init.
+ $obj->init($headers);
+
+ # Process HTTP::Headers.
  $obj->process_css;
- $obj->process(50);
+ $obj->process;
 
  # Print out.
  print "CSS\n";
@@ -255,68 +349,40 @@ Returns undef.
 
  # Output:
  # CSS
- # .gradient {
- #         height: 30px;
- #         width: 500px;
- #         background-color: red;
- #         background-image: linear-gradient(to right, red, orange, yellow, green, blue, indigo, violet);
+ # .headers {
+ #         margin: 1em;
+ #         padding: 1em;
+ #         border-radius: 8px;
+ #         box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+ #         background-color: #f4f4f4;
  # }
  # HTML
- # <div style="width: 250px;overflow: hidden;">
- #   <div class="gradient">
- #   </div>
+ # <div class="headers">
+ #   <span style="color: charTReuse;">
+ #     Host
+ #   </span>
+ #   :
+ #   <span>
+ #     example.com
+ #   </span>
+ #   <br>
+ #   </br>
+ #   <span style="color: turquoise;">
+ #     Content-Type
+ #   </span>
+ #   :
+ #   <span>
+ #     text/plain
+ #   </span>
  # </div>
-
-=head1 EXAMPLE2
-
- use strict;
- use warnings;
-
- use CSS::Struct::Output::Indent;
- use Tags::HTML::HTTP::Headers;
- use Tags::Output::Indent;
-
- if (@ARGV < 1) {
-         print STDERR "Usage: $0 percent\n";
-         exit 1;
- }
- my $percent = $ARGV[0];
-
- # Object.
- my $css = CSS::Struct::Output::Indent->new;
- my $tags = Tags::Output::Indent->new;
- my $obj = Tags::HTML::HTTP::Headers->new(
-         'css' => $css,
-         'tags' => $tags,
- );
-
- # Process indicator.
- $obj->process_css;
- $obj->process($percent);
-
- # Print out.
- print "CSS\n";
- print $css->flush."\n";
- print "HTML\n";
- print $tags->flush."\n";
-
- # Output:
- # TODO
 
 =head1 DEPENDENCIES
 
 L<Class::Utils>,
 L<Error::Pure>.
-
-=head1 SEE ALSO
-
-=over
-
-=item L<Tags::HTML::Stars>
-
-Tags helper for stars evaluation.
-
-=back
+L<List::Util>,
+L<Readonly>,
+L<Scalar::Util>.
 
 =head1 REPOSITORY
 
---
+++ examples/ex1.pl
@@ -0,0 +1,65 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use CSS::Struct::Output::Indent;
+use HTTP::Headers;
+use Tags::HTML::HTTP::Headers;
+use Tags::Output::Indent;
+
+# Object.
+my $css = CSS::Struct::Output::Indent->new;
+my $tags = Tags::Output::Indent->new;
+my $obj = Tags::HTML::HTTP::Headers->new(
+        'css' => $css,
+        'tags' => $tags,
+);
+
+# Example Header.
+my $headers = HTTP::Headers->new(
+        'Host' => 'example.com',
+        'Content-Type' => 'text/plain',
+);
+
+# Init.
+$obj->init($headers);
+
+# Process HTTP::Headers.
+$obj->process_css;
+$obj->process;
+
+# Print out.
+print "CSS\n";
+print $css->flush."\n";
+print "HTML\n";
+print $tags->flush."\n";
+
+# Output:
+# CSS
+# .headers {
+#         margin: 1em;
+#         padding: 1em;
+#         border-radius: 8px;
+#         box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+#         background-color: #f4f4f4;
+# }
+# HTML
+# <div class="headers">
+#   <span style="color: charTReuse;">
+#     Host
+#   </span>
+#   :
+#   <span>
+#     example.com
+#   </span>
+#   <br>
+#   </br>
+#   <span style="color: turquoise;">
+#     Content-Type
+#   </span>
+#   :
+#   <span>
+#     text/plain
+#   </span>
+# </div>
